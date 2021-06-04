486586a8c0beb47d5c66797a06858206955923d1
Alan <phatpazy9@gmail.com>
2021-06-04 02:03:25 -0700

    snake movement

--- source/main.c
+++ source/main.c
@@ -28,20 +28,29 @@ board = 	0x80	0x40	0x20	0x10	0x80	0x40	0x20	0x01
 
 	0xF7
 
-	0xEF
 
 
 
 
 */
-unsigned char snake_start_col = 0x02;
+unsigned char snake_start_col = 0x08;
 unsigned char snake_start_row = 0xFB;
+unsigned char tail_col = 0x01;
+unsigned char tail_row = 0x02;
 unsigned char bait_row = 0xFB;
 unsigned char bait_col = 0x08;
-unsigned char snake_row[20] = { 0xFB, 0xFB }; //chose max size
-unsigned char snake_col[20] = { 0x02, 0x01 };
-int size_of_snake = 2;
-enum snakes { start, init, up, down, right, left };
+unsigned char snake_row[20] = { 0xFB, 0xFB, 0xFB, 0xFB }; //chose max size
+unsigned char snake_col[20] = { 0x08, 0x04, 0x02, 0x01 };
+unsigned char snake_array[5][8] = { 
+				    0, 0, 0, 0, 0, 0, 0, 0,
+				    0, 0, 0, 0, 0, 0, 0, 0,
+				    0, 0, 0, 0, 1, 1, 1, 1,
+				    0, 0, 0, 0, 0, 0, 0, 0,
+				    0, 0, 0, 0, 0, 0, 0, 0,
+	
+				   }; 
+int size_of_snake = 4;
+enum snakes { start, init, up, down, right, left, wait };
 int tick(int state){
         /*
         PA0 = Up
@@ -79,13 +88,7 @@ int tick(int state){
 
                 case up:
                         if((~PINA & 0x01) == 0x01){
-                                state = up;
-                        } else if((~PINA & 0x02) == 0x02){
-                                state = down;
-                        } else if((~PINA & 0x04) == 0x04){
-                                state = left;
-                        } else if((~PINA & 0x08) == 0x08){
-                                state = right;
+                                state = wait;
                         } else {
                                 state = init;
                         }
@@ -93,14 +96,8 @@ int tick(int state){
                 break;
 
                 case down:
-                        if((~PINA & 0x01) == 0x01){
-                                state = up;
-                        } else if((~PINA & 0x02) == 0x02){
-                                state = down;
-                        } else if((~PINA & 0x04) == 0x04){
-                                state = left;
-                        } else if((~PINA & 0x08) == 0x08){
-                                state = right;
+                        if((~PINA & 0x02) == 0x02){
+                                state = wait;
                         } else {
                                 state = init;
                         }
@@ -108,40 +105,39 @@ int tick(int state){
                 break;
 
                 case left:
-                        if((~PINA & 0x01) == 0x01){
-                                state = up;
-                        } else if((~PINA & 0x02) == 0x02){
-                                state = down;
-                        } else if((~PINA & 0x03) == 0x04){
-                                state = left;
-                        } else if((~PINA & 0x08) == 0x08){
-                                state = right;
+                        if((~PINA & 0x04) == 0x04){
+                                state = wait;
                         } else {
                                 state = init;
                         }
+
 
                 break;
 
                 case right:
-                        if((~PINA & 0x01) == 0x01){
-                                state = up;
-                        } else if((~PINA & 0x02) == 0x02){
-                                state = down;
-                        } else if((~PINA & 0x04) == 0x04){
-                                state = left;
-                        } else if((~PINA & 0x08) == 0x08){
-                                state = right;
+                        if((~PINA & 0x08) == 0x08){
+                                state = wait;
                         } else {
                                 state = init;
                         }
 
                 break;
+
+		case wait:
+			if((~PINA & 0x0F) != 0x00) {
+				state = wait;
+			} else {
+				state = init;
+			}
+		break;
                 default:
                 break;
         }
         switch(state){
                 case init:
                 break;
+		case wait:
+		break;
 
                 case up:
 			if(snake_start_row == 0xFE){
@@ -207,67 +203,36 @@ int tick(int state){
                 break;
         }
 	 unsigned char old_pos_row = snake_start_row;
-        /*if((snake_start_col == bait_col) && (snake_start_row == bait_row)) {
-                //fruit eaten +1
-                size_of_snake += 1;
-                //adds one to tail end with the old position
-                snake_row[size_of_snake] = old_pos_row;
+        if((snake_start_col == bait_col) && (snake_start_row == bait_row)) {
                 bait_row = 0x00;
                 bait_col = 0x00;
-        } */      
+        }  
 
-	unsigned char snake_rows = snake_start_row;
-	for(int i = 0; i < size_of_snake; i ++){
-		//in case array contains empty elements
-		if(snake_row[i] != 0x00) {
-			snake_rows &= snake_row[i];
-		}
-	}
 
-	unsigned char snake_cols = snake_start_col;
-        for(int i = 0; i < size_of_snake; i ++){
-                //in case array contains empty elements
-                if(snake_col[i] != 0x00) {
-                       snake_cols |= snake_col[i];
-                }
-        }
-
+	/*unsigned char old_bait_col = bait_col;
+	unsigned char old_bait_row = bait_row;
 
 	if(bait_row != 0x00 & bait_col != 0x00) {
+		if(old_bait_col != bait_col && old_bait_row != bait_row){
+			if(old_bait_row != bait_row){
+				snake_rows = (snake_rows & bait_row) 
+			}
+		}
 		snake_rows = snake_rows & bait_row; //bait
 		snake_cols = snake_cols | bait_col;
+	}*/
+
+	//collision
+	for(int i = 1; i < size_of_snake; i ++) {
+		if(snake_start_row == snake_row[i] && snake_start_col == snake_col[i]) {
+			snake_cols = 0x00;
+			snake_rows = 0x00;
+			//add program end
+			PORTB = 0x10;
+		}
 	}
-        PORTC = snake_cols;
-        PORTD = snake_rows;
         return state;
 }
-//			        [can't tail block the way]
-//snake = [0][3] = [0xFE][0x08],      [0xF9][0x04] eat the bait OR [0xFB],[0xFD] 
-//				[0xFB][0x18]/[0xFB][0x0C] 
-//					[0xF3][0x04]
-//size_of_snake = 3;
-//
-//snake_row[20]; += [0xFB], [0xFD], [0xFE]
-//PORD = snake_row[i-1] & snake_row[i]
-//
-//snake_col[20]; += [0x04], [0x04], [0x04]
-//
-//
-//bait = [2][2] = [0xFB][0x04]
-//
-//left:  col = col << 1 | col;
-//right: col = col >> 1 | col;
-/*
-Board:	  8 7 6 5 4 3 2 1 0
-	0
-	1
-	2
-	3
-	4
-
-
-
- */
 int converter(){
 	/*unsigned char new_row, new _col;
 	case up:
@@ -312,6 +277,39 @@ int eat(int state) {
 
 }
 
+unsigned char loop_array[] = { 0xFE, 0xFD, 0xFB, 0xF7, 0xEF };
+unsigned char loop_2array[] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
+unsigned char stub1, stub2;
+int print(int state) {
+	for(int i = 0; i < 5; i ++){
+		for(int j = 0; j < 8; j ++) {
+			/*if(snake_array[i][j] == 1) {
+				PORTD = stub1;
+				PORTC = stub2;
+			}*/
+			for(int a = 0; a < size_of_snake; a ++){
+				if(loop_array[i] == snake_row[a] && loop_2array[j] == snake_col[a]) {
+					PORTD = loop_array[i];
+					PORTC = loop_2array[j];
+				}
+			}
+			if(loop_array[i] == bait_row && loop_2array[j] == bait_col) {
+				if(bait_row != 0x00) {
+				PORTD = bait_row;
+				PORTC = bait_col;
+				}
+			}
+		}
+        }
+	return state;
+}
+
+int display(int state){
+	//PORTD = stub1;
+	//PORTC = stub2;
+	return state;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
         DDRA = 0x00; PORTA = 0xFF;
@@ -319,8 +317,8 @@ int main(void) {
         DDRC = 0xFF; PORTC = 0x00;
         DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
-        static task task1;// task2, task3;// task4;
-        task *tasks[] = { &task1};// &task2, &task3};// &task4 };
+        static task task1, task2, task3;// task4;
+        task *tasks[] = { &task1,  &task2, &task3};// &task4 };
         const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
         const char start = 0;
         task1.state = start;
@@ -328,24 +326,23 @@ int main(void) {
         task1.elapsedTime = task1.period;
         task1.TickFct = &tick;
 
-        /*task2.state = startsp;
-        task2.period = 100;
-
+        //task2.state = startsp;
+        task2.period = 10;
         task2.elapsedTime = task2.period;
-        task2.TickFct = &tick;
+        task2.TickFct = &print;
         
-        task3.state = startL;
-        task3.period = 150;
+        //task3.state = startL;
+        task3.period = 100;
         task3.elapsedTime = task3.period;
-        task3.TickFct = &changeLock;
+        task3.TickFct = &display;
         
-        task4.state = start;
+        /*task4.state = start;
         task4.period = 10;
         task4.elapsedTime = task4.period;
         task4.TickFct = &displaySMTick;*/
 
 
-        TimerSet(100);
+        TimerSet(10);
         TimerOn();
 
         unsigned short i;
